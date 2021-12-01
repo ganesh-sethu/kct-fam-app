6768cad638ab2e51968c267c3278368f2f5f6614
subash7038 <chsubash333@gmail.com>
2021-12-02 00:04:53 +0530

    :bulb:[Added] jwt authentication and more

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
-//2 
+const express = require("express");
+const app = express();
+const db = require("./db/db");
+const cors = require("cors");
+require("dotenv").config();
 
-const express= require('express');
-const app =express();
+const PORT = process.env.SERVER_PORT;
+const corsOption = {
+  origin: process.env.FRONT_END_DOMAIN,
+};
 
-PORT = 3000|| process.env.PORT;
+db.connect();
 
-app.get("/",(req,res)=>{
-res.send("om \n welcome to kct fam app");
-})
-
-app.listen(PORT,()=>{
-    
-    console.log(`app running on port ${PORT} successfully`);
+app.use(cors(corsOption));
+app.use(express.json());
+app.use("/api/admin", require("./routes/adminRoute"));
+app.use("/api", require("./routes/authenticateRoute"));
+app.get("/", (req, res) => {
+  res.send("welcome to kct fam app");
 });
 
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT} successfully`);
+});
