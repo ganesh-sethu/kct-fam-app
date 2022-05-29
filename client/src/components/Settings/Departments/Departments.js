526100801ddd9b3be7df97472689f26b111f8e43
subash7038 <chsubash333@gmail.com>
2022-05-29 19:37:11 +0530

    add :settings- departments ui

--- client/src/components/Settings/Departments/Departments.js
+++ client/src/components/Settings/Departments/Departments.js
@@ -1,31 +1,67 @@
 import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import TableCell , { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { styled } from "@mui/material/styles";
+import { Button } from '@mui/material';
+import EditModal from "./EditModal"
+import AddModal from "./AddModal"
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+
 
 export default function BasicTable({departments}) {
+  const [editModal, setEditModal] = React.useState(false);
+  const [addModal, setAddModal] = React.useState(false);
+  const [department,setDepartment] = React.useState([])
+
+  const handleEditDepartment = (dept) => {
+    setEditModal(true)
+    setDepartment(dept)
+
+  }
+
+  const handleNewDept = () => {
+    setAddModal(true)
+
+  }
   const labels = ["S.No","Department","Allocated Budget","Used Budget"]
-  console.log(departments)
   return (
-    <TableContainer component={Paper}>
+    <div>
+      <div style={{width:"100%",display:"flex",flexDirection:"row-reverse",marginBottom:"1rem"}}>
+        <Button style={{width:"120px"}} variant="contained" color="primary" onClick={handleNewDept}>New</Button>
+      </div>
+      <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             {labels.map((item,i) => {
-              return <TableCell align="center" key={i}>{item}</TableCell>
+              return <StyledTableCell align="center" key={i}>{item}</StyledTableCell>
             })}
-
           </TableRow>
         </TableHead>
         <TableBody>
           {departments.map((row,i) => (
             <TableRow
+              hover
               key={i}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              onClick={() => handleEditDepartment(row)}
+              style={{cursor:"pointer"}}
+    
             >
               <TableCell component="th" scope="row" align="center">
                 {i+1}
@@ -37,6 +73,13 @@ export default function BasicTable({departments}) {
           ))}
         </TableBody>
       </Table>
+      <EditModal editModal={editModal} setEditModal={setEditModal} department={department}/>
+      <AddModal addModal={addModal} setAddModal={setAddModal} />
     </TableContainer>
+    
+    </div>
+    
   );
 }
+
+
