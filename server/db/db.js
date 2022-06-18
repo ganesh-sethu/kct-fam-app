3056689583ea5600760eb5afd0c43b7dd8835c17
Aswath S <aswath.18cs@kct.ac.in>
2022-06-18 14:04:04 +0000

    Refactor. Try. Change db connection mechanism

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,21 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const baseConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
+}
+
+const database = process.env.DB_DATABASE
+
+const tempConnection = mysql.createConnection(baseConfig)
+
+tempConnection.query(`CREATE DATABASE IF NOT EXISTS ${database};`)
+
+const connection = mysql.createConnection({
+  ...baseConfig,
+  database,
 });
 
 module.exports = connection;
